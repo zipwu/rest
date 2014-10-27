41ee51dd38ddd3d9155609de34e28f3cbb65b786
happiness123 <yuliang_furture@126.com>
2014-10-27 20:45:59 +0800

    Update FutureClient.java

--- JAVA/OkCoinRESTAPI/src/com/okcoin/rest/FutureClient.java
+++ JAVA/OkCoinRESTAPI/src/com/okcoin/rest/FutureClient.java
@@ -24,7 +24,7 @@ public class FutureClient {
 		String secret_key = "";
 	    
 		//请求URL 国际站https://www.okcoin.com
-	    //       国内站https://www.okcoin.cn
+	        // 国内站https://www.okcoin.cn
 		
 		String url_prex = "";
 		
@@ -42,48 +42,44 @@ public class FutureClient {
 		*/
 		IFutureRestApi futurePostV1 = new FutureRestApiV1(url_prex, partner,secret_key);
 
-		System.out.println("----------期货行情信息-------");
-		System.out.println(futureGetV1.future_ticker("btc_usd", "this_week"));
+	        //期货行情信息
+		futureGetV1.future_ticker("btc_usd", "this_week");
 
-		System.out.println("-----------期货指数信息-------");
-		System.out.println(futureGetV1.future_index("btc_usd"));
+		//期货指数信息
+		futureGetV1.future_index("btc_usd");
 
-		System.out.println("----------期货交易信息-------");
-		System.out.println( futureGetV1.future_trades("btc_usd", "this_week"));
+		//期货交易信息
+		futureGetV1.future_trades("btc_usd", "this_week");
 
-		System.out.println("----------期货市场深度-------");
-		System.out.println(futureGetV1.future_depth("btc_usd", "this_week"));
+		//期货市场深度
+		futureGetV1.future_depth("btc_usd", "this_week");
 
-		System.out.println("----------美元-人民币汇率-------");
-		System.out.println(futureGetV1.exchange_rate());
+		//美元-人民币汇率
+		futureGetV1.exchange_rate();
 
-		System.out.println("----------期货下单-------");
+		//期货下单
 		String tradeResultV1 = futurePostV1.future_trade("btc_usd","this_week", "10.134", "1", "1", "0");
 		JSONObject tradeJSV1 = JSONObject.parseObject(tradeResultV1);
 		String tradeOrderV1 = tradeJSV1.getString("order_id");
-
 		System.out.println(tradeResultV1);
 
+		//期货用户订单查询
+		futurePostV1.future_order_info("btc_usd", "this_week",tradeOrderV1, "1", "1", "2");
 
-		System.out.println("----------期货用户订单查询-------");
-		System.out.println(futurePostV1.future_order_info("btc_usd", "this_week",tradeOrderV1, "1", "1", "2"));
+		//取消订单
+		futurePostV1.future_cancel("btc_usd", "this_week",tradeOrderV1);
 
+		//期货账户信息
+		futurePostV1.future_userinfo();
 
-		System.out.println("----------取消订单-------");
-		System.out.println(futurePostV1.future_cancel("btc_usd", "this_week",tradeOrderV1));
+		//逐仓期货账户信息
+		futurePostV1.future_userinfo_4fix();
+		
+		//期货用户持仓查询
+		futurePostV1.future_position("btc_usd", "this_week");
 
-		//
-		System.out.println("--------期货账户信息-------");
-		System.out.println( futurePostV1.future_userinfo());
-
-		System.out.println("--------逐仓期货账户信息-------");
-		System.out.println( futurePostV1.future_userinfo_4fix());
-		//
-		System.out.println("----------期货用户持仓查询-------");
-		System.out.println(futurePostV1.future_position("btc_usd", "this_week"));
-
-		System.out.println("----------期货用户逐仓持仓查询-------");
-		System.out.println( futurePostV1.future_position_4fix("btc_usd", null));
+		//期货用户逐仓持仓查询
+	        futurePostV1.future_position_4fix("btc_usd", null);
 
 
 	}
