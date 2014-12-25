9d4af11529c2e99850df5d98ee5de80f3f62238a
happiness123 <yuliang_furture@126.com>
2014-12-25 11:55:23 +0800

    Update OkTrade.php

--- php/OkTrade.php
+++ php/OkTrade.php
@@ -22,7 +22,7 @@ class OkAPI {
 	$sign=md5 ($post_data."&secret_key=".$this->secretKey );
 	$sign=strtoupper($sign);
 	var_dump($sign);
-	$post="api_key=".$this->apiKey."&sign=".$sign."&".$post_data;
+	$post=.$post_data."&sign=".$sign;
 	var_dump($post);
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
